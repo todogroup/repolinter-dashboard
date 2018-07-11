dc3a8346e7518302e00f90108e3fee2882c506f4
ab-hay <abhaydeepsin@umass.edu>
2018-07-10 17:04:15 -0700

    server side for webhooks, Api endpoint handler for code pushes

--- src/serverless/githubUtils/app.js
+++ src/serverless/githubUtils/app.js
@@ -1,6 +1,5 @@
 const octokit = require('@octokit/rest')();
 
-
 const db = require('./db');
 const repolinter = require('./repolinter-runner');
 
@@ -41,3 +40,19 @@ exports.removeExistingOrg = async (event,context)=>{
   await db.removeOrg(event.pathParameters.org);
 }
  
+exports.webhookHandler = async (event, context)=>{
+  // setting up the env
+  process.env.PATH = process.env.LAMBDA_TASK_ROOT + "/bin/usr/bin:" + process.env.PATH;
+  process.env.GIT_EXEC_PATH = process.env.LAMBDA_TASK_ROOT + '/bin/usr/libexec/git-core';
+  const eventData = JSON.parse(event.body);
+  const element = eventData.repository;
+  const results = await repolinter.lintRepo(element.clone_url);
+  //insert or update query
+  await db.upsert(element.owner.login, element.id, element.name, element.full_name, element.owner, element.collaborators_url, element.teams_url,
+    element.created_at, element.updated_at, element.git_url, element.ssh_url, element.clone_url, element.language, element.watchers_count,
+    element.open_issues_count, element.license, JSON.stringify(results));
+  let response = {
+    'statusCode': 200,
+  }
+  return response;
+}
--- src/serverless/githubUtils/db.js
+++ src/serverless/githubUtils/db.js
@@ -21,7 +21,7 @@ function connect(){
 const postData = async function (org_name, github_id, repo_name, full_name, github_owner_details, collaborators_url, teams_url, created_at, updated_at, git_url, ssh_url, clone_url, language, watchers_count, open_issues_count, license, repolinter_scan){
 await pg.query('INSERT INTO githubrepos '+ 
 	'(org_name, github_id, repo_name, full_name, github_owner_details, collaborators_url, teams_url, created_at, updated_at, git_url, ssh_url, clone_url, language, watchers_count, open_issues_count, license, repolinter_scan)' +
-	' values ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16, $17)',
+	'values ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16, $17)',
 	[org_name, github_id, repo_name, full_name, github_owner_details, collaborators_url, teams_url, created_at, updated_at, git_url, ssh_url, clone_url, language, watchers_count, open_issues_count, license, repolinter_scan]).catch(error => console.error(error));
 }
 
@@ -37,4 +37,12 @@ const deleteData = async function(org_name){
 await pg.query('delete from githubrepos where org_name = ($1);',[org_name]);
 }
 
-module.exports = {postData :postData, pg, addOrg, removeOrg, deleteData};
+const upsert = async function(org_name, github_id, repo_name, full_name, github_owner_details, collaborators_url, teams_url, created_at, updated_at, git_url, ssh_url, clone_url, language, watchers_count, open_issues_count, license, repolinter_scan){
+await pg.query('INSERT INTO githubrepos '+ 
+    '(org_name, github_id, repo_name, full_name, github_owner_details, collaborators_url, teams_url, created_at, updated_at, git_url, ssh_url, clone_url, language, watchers_count, open_issues_count, license, repolinter_scan) ' +
+    'values ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16, $17) ON CONFLICT (repo_name) DO UPDATE SET '+
+    'org_name=($1), github_id=($2), full_name=($4), github_owner_details=($5), collaborators_url=($6), teams_url=($7), created_at=($8), updated_at=($9), git_url=($10), ssh_url=($11), clone_url=($12), language=($13), watchers_count=($14), open_issues_count=($15), license=($16), repolinter_scan=($17);',
+    [org_name, github_id, repo_name, full_name, github_owner_details, collaborators_url, teams_url, created_at, updated_at, git_url, ssh_url, clone_url, language, watchers_count, open_issues_count, license, repolinter_scan]).catch(error => console.error(error));
+}
+
+module.exports = {postData :postData, pg, addOrg, removeOrg, deleteData, upsert};
